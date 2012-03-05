4fc5af239a124c0afae37d52fb72e05aee477f1e
Lecroart Francois <peernohell@gmail.com>
2012-03-05 13:18:49 +0100

    move ping code in ping.js move monitor code in monitor.js add in netmon object NetworkMonitor to check differante element (computer, webserver, ...)

--- server.js
+++ server.js
@@ -2,40 +2,92 @@ var app = require('http').createServer(handler).listen(8000)
 	, io = require('socket.io').listen(app)
 	, fs = require('fs')
 	, spawn = require('child_process').spawn
-	, util = require('util')
-	, ON = "ON"
-	, OFF = "OFF"
-	, pingState = OFF;
+	, NM = require('./lib/netmon').NetworkMonitor;
 
+monitor = new NM();
 
-
-ping = spawn('ping', ['www.google.com']);
-pingState = ON;
 
 io.sockets.on('connection', function (socket) {
 
-	socket.emit('news', "welcome. Ping is currently " + pingState);
+	// connect action event
+	socket.on('action', function (action) {
+		action = action.toUpperCase();
+		console.log('action : ' + action);
+		console.log('monitor state : ' + monitor.state);
+		if (action === 'START') {
+
+			if (monitor.state === 'START') {
+				socket.emit('update', "monitor already started!");
+				return;
+			}
+
+			if (monitor.state === 'PAUSE') {
+				socket.emit('update', "monitor are pause. juste resume it");
+				monitor.resume();
+
+			} else {
+				monitor.start();
+			}
+		}
+		if (action === 'PAUSE') {
+			if (monitor.state === 'PAUSE') {
+				socket.emit('update', "monitor already paused!");
+				return;
+			}
+			if (monitor.state === 'STOP') {
+				socket.emit('update', "monitor can't pause. monitor are stoped!");
+				return;
+			}
+			monitor.pause();
+		}
+		if (action === 'RESUME') {
+			if (monitor.state !== 'PAUSE') {
+				socket.emit('update', "monitor can only be resume if it's in pause. currently monitor are " + monitor.state);
+				return;
+			}
+			monitor.resume();
+		}
+
+		if (action === 'STOP') {
+			if (monitor.state === 'STOP') {
+				socket.emit('update', "monitor already stoped!");
+				return;
+			}
+			monitor.stop();
+		}
+	});
+
+	socket.emit('news', "welcome. monitor is currently " + monitor.state);
 	function output (data) {
-		console.log('ping : %s', data);
-		socket.emit('update', data.toString());
-		//socket.emit('update', 'ping emit!');
+		console.log('monitor : %s', data);
+		socket.emit('update', data);
 	}
 
-	if (pingState === ON) {
-		ping.stdout.on("data", output);
-
-		socket.on("disconnect", function (socket) {
-			ping.stdout.removeListener("data", output);
-		});
+	function stateupdate (data) {
+		socket.emit('news', data);
 	}
+
+	monitor.on('state', stateupdate);
+	
+	if (monitor.state === 'STOP') {
+		monitor.start();
+	} else if (monitor.state === 'PAUSE') {
+		monitor.resume();
+	}
+
+	monitor.on("data", output);
+
+	socket.on("disconnect", function (socket) {
+		monitor.removeListener("data", output);
+		monitor.removeListener("state", stateupdate);
+	});
 
 });
 
-// send to every websocket that the ping is off 
-ping.on('exit', function () {
-	console.log('ping exit');
-	pingState = OFF;
-	io.sockets.emit('news', "ping terminated");
+// send to every websocket that the monitor is off 
+monitor.on('exit', function () {
+	console.log('monitor exit');
+	io.sockets.emit('news', "monitor terminated");
 });
 
 
@@ -54,5 +106,5 @@ function handler (req, res) {
 }
 
 app.on('close', function () {
-	ping.stdin.end();
+	monitor.stdin.end();
 });
