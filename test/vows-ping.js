4426d9137c9128bd2a9088dc32efb253c1ec59b9
Sébasten Dolard <sdolard@gmail.com>
2012-03-08 09:08:50 +0100

    Ping + ping vows test

--- test/vows-ping.js
+++ test/vows-ping.js
@@ -2,17 +2,73 @@ var
 vows = require('vows'),
 assert = require('assert'),
 util = require('util'),
-ping = require('../lib/ping'),
-localhostPing;
+events = require("events"),
 
+ping = require('../lib/ping'),
+start,
+end;
 
 exports.suite1 = vows.describe('ping').addBatch({
-    'When we ping localhost': {
-	topic: function () {
-		localhostPing = ping.create('localhost'); 
-		localhostPing.star();
-	},
-	'We succeed': function (error, stdout, stderr) {
-	}
-    }
+		'When we ping localhost': {
+			topic: function() {
+				var promise = new events.EventEmitter();
+				
+				ping.check('localhost', function (r) {
+						promise.emit('success', r);
+				});
+				return promise;
+			},
+			'We succeed': function (r) {
+				assert.equal(r.host, 'localhost');
+				assert.equal(r.exitCode, 0);
+			}
+		},
+		'When we ping nohost': {
+			topic: function() {
+				var promise = new events.EventEmitter();
+				
+				ping.check('nohost', function (r) {
+						promise.emit('success', r);
+				});
+				return promise;
+			},
+			'We failed': function (r) {
+				assert.equal(r.host, 'nohost');
+				assert.notEqual(r.exitCode, 0);
+			}
+		},
+		'When we ping ::1': {
+			topic: function() {
+				var promise = new events.EventEmitter();
+				
+				ping.check('::1', true, function (r) {
+						promise.emit('success', r);
+				});
+				return promise;
+			},
+			'We succeed': function (r) {
+				assert.equal(r.host, '::1');
+				assert.equal(r.exitCode, 0);
+				
+			}
+		},
+		'When we ping 1.1.1.1 with timeout set to 2s': {
+			topic: function() {
+				var  
+				promise = new events.EventEmitter();
+				start = new Date();
+				
+				ping.check('1.1.1.1', 2, function (r) {
+						end = new Date();
+						promise.emit('success', r);
+						
+				});
+				return promise;
+			},
+			'It takes 2s to return': function (r) {
+				assert.equal(r.host, '1.1.1.1');
+				assert.notEqual(r.exitCode, 0);
+				assert.equal(end.getTime() - start.getTime() >= 2000, true);
+			}
+		}
 });
