a3e3b010d1a448f9132ac8adbc9f48fec57e8187
Sébastien Dolard <sdolard@gmail.com>
2012-05-18 11:14:16 +0200

    First stable engin e version

--- bin/netmon.js
+++ bin/netmon.js
@@ -1,8 +1,26 @@
 var 
+//node
+util = require('util'),
+
+// lib
 configLoader = require('../lib/config'),
 netjob = require('../lib/netjob'),
+
+// global var
 configFile = __dirname + "/config.default.json";
 
+function onTaskResult(/*Error*/err, /*Object*/data, /*NetTask*/task) {
+	if (err) {
+		console.log('%s: %s on %s failed', task.id, task.action, task.host);
+	} else {
+		console.log('%s: %s on %s succeed', task.id, task.action, task.host);
+	}
+	//console.log('TASK RESULT: id: %s: %s', task.id, util.inspect(data, false, 100));
+}
+
+function onJobDone(task) {
+	console.log('JOB DONE: id: %s', task.id);
+}
 
 configLoader.load(configFile, function(err, config){
 		var i = 0,
@@ -18,8 +36,9 @@ configLoader.load(configFile, function(err, config){
 		}
 		
 		for (i = 0; i < config.monitor.length; i++){
-			//debugger;
 			netJob = netjob.create(config.monitor[i]);
+			netJob.on('result', onTaskResult);
+			netJob.on('done', onJobDone);
 		}
 });
 
