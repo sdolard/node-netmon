e42f352e4ba05479c17aecc38314392878af4e1b
Sébasten Dolard <sdolard@gmail.com>
2012-06-14 22:53:58 +0200

    more vows test and fix

--- test/vows-nettasq.js
+++ test/vows-nettasq.js
@@ -6,6 +6,83 @@ events = require("events"),
 nettasq = require('../lib/nettask');
 
 exports.suite1 = vows.describe('nettask').addBatch({
+		'When create a empty task': {
+			topic: function() {
+				var
+				promise = new events.EventEmitter(),
+				invalidPingTask = nettasq.create();
+				
+				invalidPingTask.on('result', function (err, r, task) {
+						if (err) { 
+							promise.emit('success', err); 
+						}
+				});
+				invalidPingTask.run();
+				return promise;
+			},
+			'It failed': function (err) {
+				assert.isNotNull(err);
+			},
+			'Error code equal EINVALIDACTION': function (err) {
+				assert.strictEqual(err.code, 'EINVALIDACTION');
+			},
+			'Error message equal "action is undefined"': function (err) {
+				assert.strictEqual(err.message, 'action is undefined');
+			}
+		},
+		'When create a ping task with no config': {
+			topic: function() {
+				var
+				promise = new events.EventEmitter(),
+				invalidPingTask = nettasq.create({
+						action: 'ping'
+				});
+				
+				invalidPingTask.on('result', function (err, r, task) {
+						if (err) { 
+							promise.emit('success', err); 
+						}
+				});
+				invalidPingTask.run();
+				return promise;
+			},
+			'It failed': function (err) {
+				assert.isNotNull(err);
+			},
+			'Error code equal EINVALIDCONFIG': function (err) {
+				assert.strictEqual(err.code, 'EINVALIDCONFIG');
+			},
+			'Error message equal "config is undefined"': function (err) {
+				assert.strictEqual(err.message, 'config is undefined');
+			}
+		},  
+		'When create a ping task with no config.host': {
+			topic: function() {
+				var
+				promise = new events.EventEmitter(),
+				invalidPingTask = nettasq.create({
+						action: 'ping',
+						config: {}
+				});
+				
+				invalidPingTask.on('result', function (err, r, task) {
+						if (err) { 
+							promise.emit('success', err); 
+						}
+				});
+				invalidPingTask.run();
+				return promise;
+			},
+			'It failed': function (err) {
+				assert.isNotNull(err);
+			},
+			'Error code equal EINVALIDCONFIGHOST': function (err) {
+				assert.strictEqual(err.code, 'EINVALIDCONFIGHOST');
+			},
+			'Error message equal "config.host is undefined"': function (err) {
+				assert.strictEqual(err.message, 'config.host is undefined');
+			}
+		},
 		'When create a valid ping action task on localhost': {
 			topic: function() {
 				var
@@ -18,11 +95,11 @@ exports.suite1 = vows.describe('nettask').addBatch({
 				});
 				
 				pingTask.on('result', function (err, r, task) {
-					if (err) { 
-						promise.emit('error', err); 
-					} else { 
-						promise.emit('success', r); 
-					}
+						if (err) { 
+							promise.emit('error', err); 
+						} else { 
+							promise.emit('success', r); 
+						}
 				});
 				pingTask.run();
 				return promise;
@@ -46,6 +123,27 @@ exports.suite1 = vows.describe('nettask').addBatch({
 			},
 			'data is a string': function (err, r) {
 				assert.isString(r.data);
+			}
+		},
+		'When a valid ping action task on localhost is finished': {
+			topic: function() {
+				var
+				promise = new events.EventEmitter(),
+				pingTask = nettasq.create({
+						action: 'ping',
+						config: {
+							host: 'localhost'
+						}
+				});
+				
+				pingTask.on('done', function (task) {
+						promise.emit('success', task); 
+				});
+				pingTask.run();
+				return promise;
+			},
+			'"done" event is called': function (err, task) {
+				assert.strictEqual(task.config.exitCode, 0);
 			}
 		}
 });
